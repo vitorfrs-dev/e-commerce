10ffbd787fd6aa8711e0ab2890e5bbbe664bb33b
vitorfrs-dev <vitorfrs98@gmail.com>
2019-01-07 23:19:20 -0200

    Paginação

--- vendor/hcodebr/php-classes/src/Model/Category.php
+++ vendor/hcodebr/php-classes/src/Model/Category.php
@@ -115,6 +115,38 @@ class Category extends Model {
         
     }
 
+    /*Função que auxilia na paginação */
+    public function getProductsPage($page = 1, $itemsPerPage = 3)
+    {
+
+        $sql = new Sql();
+
+        $start = ($page - 1) * $itemsPerPage;
+
+        $results = $sql->select(
+            "SELECT SQL_CALC_FOUND_ROWS * 
+                FROM tb_products a
+                INNER JOIN tb_productscategories b ON a.idproduct = b.idproduct
+                INNER JOIN tb_categories c ON c.idcategory = b.idcategory
+                WHERE c.idcategory = :idcategory
+                LIMIT $start, $itemsPerPage;
+            
+            ", [
+                ":idcategory" => $this->getidcategory()
+        ]);
+        
+        $resultTotal = $sql->select("SELECT FOUND_ROWS() AS nrtotal; ");
+
+        return [
+            'data' =>Product::checkList($results),
+            'total' => (int)$resultTotal[0]['nrtotal'],
+            'pages' => ceil($resultTotal[0]['nrtotal'] / $itemsPerPage)
+        ];
+
+
+
+    }
+
 
     public function addProduct(Product $product)
     {
@@ -141,6 +173,7 @@ class Category extends Model {
         ]);
 
     }
+
     
 }
 
