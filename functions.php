afddfedb969b30d86f4591c97ca00be942e63e62
vitorfrs-dev <vitorfrs98@gmail.com>
2019-02-03 21:39:39 -0200

    Site Login

--- functions.php
+++ functions.php
@@ -1,8 +1,27 @@
 <?php
 
+use \Hcode\Model\User;
+use \Hcode\Model\Cart;
+
 function formatPrice(float $vlprice) {
 
     return number_format($vlprice, 2, ",", ".");
+
+}
+
+function checkLogin($inadmin = true)
+{
+
+    return User::checklogin($inadmin);
+
+}
+
+function getUserName()
+{
+
+    $user = User::getFromSession();
+
+    return $user->getdesperson();
 
 }
 
