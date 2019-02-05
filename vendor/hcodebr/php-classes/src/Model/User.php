b84f8d45febd74e1b5024747efd4018ad807c52a
vitorfrs-dev <vitorfrs98@gmail.com>
2019-02-05 21:45:19 -0200

    Esqueceu a Senha - Site

--- vendor/hcodebr/php-classes/src/Model/User.php
+++ vendor/hcodebr/php-classes/src/Model/User.php
@@ -198,7 +198,7 @@ class User extends Model {
     }
 
 
-    public static function getForgot($email)
+    public static function getForgot($email, $inadmin = true)
     {
 
         $sql = new Sql();
@@ -239,7 +239,19 @@ class User extends Model {
                 $code = base64_encode(mcrypt_encrypt(MCRYPT_RIJNDAEL_128, User::SECRET, $dataRecovery['idrecovery'], 
                     MCRYPT_MODE_ECB));
 
-                $link = "http://www.hcodecommerce.com.br/admin/forgot/reset?code=$code";
+                if ($inadmin === true) {
+
+                    $link = "http://www.hcodecommerce.com.br/admin/forgot/reset?code=$code";
+
+                } else {
+
+                    $link = "http://www.hcodecommerce.com.br/forgot/reset?code=$code";
+
+                }
+
+
+
+                
                 
                 $mailer = new Mailer($data['desemail'], $data['desperson'], "Redefinir Senha da Hcode Store",
                 "forgot", array(
